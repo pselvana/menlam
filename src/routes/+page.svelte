473818b08d54968ecc0fd5168f7fd1e84bc383ed
Piranavan Selvanandan <pirana@gmail.com>
2025-11-15 22:44:05 +0800

    New favicon and project on landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="hero min-h-screen bg-base-100">
 	<div class="hero-content text-center">
 		<div class="max-w-xl">
-			<h1 class="mb-6 text-5xl font-bold">Menlam</h1>
+			<h1 class="mb-6 text-5xl font-bold">Project Menlam</h1>
 			<p class="mb-6 text-lg">
 				A trusted offline medical reference for Bhutanese doctors.<br />
 				Fast. Reliable. Available anywhere.
